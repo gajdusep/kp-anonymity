ea9b97fae8b41220be055525d67f8811758af74c
gabriele De leo <deleo.gabriele97@gmail.com>
2020-12-28 16:06:41 +0100

    added k-anonimity top down

--- src/kp_anonymity.py
+++ src/kp_anonymity.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random 
 
 from group import *
 from node import *
@@ -58,7 +59,93 @@ def do_kp_anonymity():
     table_group.add_row_to_group(row)
     print('a row was added to the table', table_group.shape())
 
-    # -----------------------------------------------
+
+
+
+def group_partition(G: Group):
+    Gu = create_empty_group()
+    Gv = create_empty_group()
+    u_max = G.get_random_row() 
+    
+    for i in range(3):
+        max_ncp = 0
+        for i in range(G.size):
+            v = G.get_row_at_index_(i)
+            tmp = create_empty_group()
+            tmp.add_row_to_group(u_max)
+            tmp.add_row_to_group(v)
+            tmp_min_max_diff = tmp.get_min_max()
+            ncp = compute_ncp(tmp.group_table, tmp_min_max_diff)
+            if ncp>max_ncp:
+                max_ncp = ncp
+                v_max = v
+                index_v = i
+   
+        max_ncp = 0
+        for i in range(G.size):
+            u = G.get_row_at_index_(i)
+            tmp = create_empty_group()
+            tmp.add_row_to_group(v_max)
+            tmp.add_row_to_group(u)
+            tmp_min_max_diff = tmp.get_min_max()
+            ncp = compute_ncp(tmp.group_table, tmp_min_max_diff)
+            if ncp > max_ncp:
+                max_ncp = ncp
+                u_max = u
+                index_u = i
+
+    Gu.add_row_to_group(u_max)
+    Gv.add_row_to_group(v_max)
+
+    for i in random.sample(range(G.size), G.size):
+        if i == index_u or i == index_v:
+            continue
+        else: 
+         w = G.get_row_at_index(i)
+         tmp_Gu = Gu
+         tmp_Gu.add_row_to_group(w)
+         tmp_Gu_min_max_diff = tmp_Gu.get_min_max()
+         ncp_Gu = compute_ncp(tmp_Gu.group_table, tmp_Gu_min_max_diff)
+
+         tmp_Gv = Gv
+         tmp_Gv.add_row_to_group(w)
+         tmp_Gv_min_max_diff = tmp_Gv.get_min_max()
+         ncp_Gv = compute_ncp(tmp_Gv.group_table, tmp_Gv_min_max_diff)
+
+         if ncp_Gu < ncp_Gv:
+                Gu.add_row_to_group(w)
+         else:
+                Gv.add_row_to_group(w)
+
+    return group_list = [Gu,Gv] 
+
+         
+    
+
+def k_anonymity_top_down(table_group: Group,k: int):
+        if table_group.size()<= k:
+            return 
+        else:
+            group_list = group_partition(table_group)
+            is_anonymized = False         
+            while (!is_anonymized):
+                is_anonymized = True
+                for i in group_list:
+                    tmp_list = []
+                    if group_list[i].size > k:
+                        tmp_list.extend(group_partition(group_list[i]))
+                        is_anonymized = False
+                    else:
+                        tmp_list.append(group_list[i]) 
+                group_list = tmp_list
+            return group_list       
+               
+
+
+
+
+
+# -----------------------------------------------
     # example of ncp computing
     # - will be needed in k anonymity
     # - (delete this, when you start coding k-anonymity)
