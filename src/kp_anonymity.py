0f8b2f1c5defe84d601a500ab91edcbd631392cb
DESKTOP-MARN2KV\Gabriele <deleo.gabriele97@gmail.com>
2021-01-08 22:08:21 +0100

    resolve conflict

--- src/kp_anonymity.py
+++ src/kp_anonymity.py
@@ -163,8 +163,116 @@ def do_kp_anonymity(path_to_file: str, k: int):
     for ag in anonymized_groups:
         print('shape:', ag.shape(), '; company codes:', ag.ids)
 
+<<<<<<< Updated upstream
     visualize_intervals(anonymized_groups)
 
+=======
+<<<<<<< HEAD
+def group_partition(G: Group):
+    size = G.size()
+    Gu = create_empty_group()
+    Gv = create_empty_group()
+    u_max = G.get_random_row() 
+    
+    for i in range(3):
+        max_ncp = 0
+        for i in range(size):
+            v = G.get_row_at_index_(i)
+            tmp = create_empty_group()
+            tmp.add_row_to_group(u_max)
+            tmp.add_row_to_group(v)
+            tmp_min_max_diff = tmp.get_min_max()
+            ncp = compute_ncp(tmp.group_table, tmp_min_max_diff)
+            if ncp>max_ncp:
+                max_ncp = ncp
+                v_max = v
+                index_v = i
+   
+        max_ncp = 0
+        for i in range(size):
+            u = G.get_row_at_index_(i)
+            tmp = create_empty_group()
+            tmp.add_row_to_group(v_max)
+            tmp.add_row_to_group(u)
+            tmp_min_max_diff = tmp.get_min_max()
+            ncp = compute_ncp(tmp.group_table, tmp_min_max_diff)
+            if ncp > max_ncp:
+                max_ncp = ncp
+                u_max = u
+                index_u = i
+
+    Gu.add_row_to_group(u_max)
+    Gv.add_row_to_group(v_max)
+
+    for i in random.sample(range(size), size):
+        if i == index_u or i == index_v:
+            continue
+        else: 
+            w = G.get_row_at_index(i)
+
+            tmp_Gu = Gu
+            tmp_Gu.add_row_to_group(w)
+            tmp_Gu_min_max_diff = tmp_Gu.get_min_max()
+            ncp_Gu = compute_ncp(tmp_Gu.group_table, tmp_Gu_min_max_diff)
+
+            tmp_Gv = Gv
+            tmp_Gv.add_row_to_group(w)
+            tmp_Gv_min_max_diff = tmp_Gv.get_min_max()
+            ncp_Gv = compute_ncp(tmp_Gv.group_table, tmp_Gv_min_max_diff)
+
+            if ncp_Gu < ncp_Gv:
+                Gu.add_row_to_group(w)
+            else:
+                Gv.add_row_to_group(w)
+
+    group_list = [Gu,Gv]
+    return group_list 
+
+         
+    
+
+def k_anonymity_top_down(table_group: Group,k: int):
+        if table_group.size()<= k:
+            return 
+        else:
+            group_list = group_partition(table_group)
+            is_anonymized = False         
+            while (!is_anonymized):
+                is_anonymized = True
+                for i in group_list:
+                    tmp_list = []
+                    if group_list[i].size > k:
+                        tmp_list.extend(group_partition(group_list[i]))
+                        is_anonymized = False
+                    else:
+                        tmp_list.append(group_list[i]) 
+                group_list = tmp_list
+            return group_list       
+               
+
+
+
+
+
+# -----------------------------------------------
+    # example of ncp computing
+    # - will be needed in k anonymity
+    # - (delete this, when you start coding k-anonymity)
+    # -----------------------------------------------
+    print('---ncp computing---')
+    new_table = create_empty_group()
+    print('empty group created:', new_table.shape())
+    row_1 = table_group.get_row_at_index(2)
+    row_2 = table_group.get_row_at_index(3)
+    new_table.add_row_to_group(row_1)
+    new_table.add_row_to_group(row_2)
+    print('new rows added:', new_table.shape())
+    new_table_ncp = compute_ncp(new_table.group_table, min_max_diff=table_min_max_diff)
+    print('ncp computed:', new_table_ncp)
+=======
+    visualize_intervals(anonymized_groups)
+
+>>>>>>> Stashed changes
     # row = table_group.get_row_at_index(3)
     # print('a row is a vector (numpy array with one dimension)', row.shape)
     # print('   - last row:', table_group.get_row_at_index(table_group.size() - 1))
@@ -174,6 +282,10 @@ def do_kp_anonymity(path_to_file: str, k: int):
     # table_group.delete_last_added_row()
     # print('a row was deleted from the table', table_group.shape())
     # print('   - last row:', table_group.get_row_at_index(table_group.size() - 1))
+<<<<<<< Updated upstream
+=======
+>>>>>>> 9ff2afe22462085d5bc89e44452a6606f5a37449
+>>>>>>> Stashed changes
 
 
 if __name__ == "__main__":
