4ea300b4384a2a5ceed6f99c28c59a0f952dcb0a
gajdusep <gajdusek.pavel@gmail.com>
2021-01-26 16:48:25 +0100

    finished (almost)

--- src/k_anonymity.py
+++ src/k_anonymity.py
@@ -296,8 +296,10 @@ def find_smallest_group_index(list_of_groups: List[Group]) -> int:
         range(len(list_of_groups)),
         key=lambda i: list_of_groups[i].size())
 
+
 def find_biggest_group(list_of_groups: List[Group]) -> Group:
     return max(list_of_groups,  key=lambda group: group.size())
+
 
 def find_biggest_group_index(list_of_groups: List[Group]) -> int:
     return max(range(len(list_of_groups)), key=lambda i: list_of_groups[i].size())
@@ -316,19 +318,14 @@ def k_anonymity_bottom_up(table_group: Group, k: int) -> List[Group]:
         list_of_groups.append(group_with_single_tuple)
 
     # do k-anonymity on groups
-    while find_smallest_group(list_of_groups).size() < k:
+    smallest_group_i = find_smallest_group_index(list_of_groups)
+    while list_of_groups[smallest_group_i].size() < k:
+        group_to_check = list_of_groups.pop(smallest_group_i)
+
+        index_of_merging_group = find_index_of_group_to_be_merged(group_to_check, list_of_groups, min_max_diff)
+        list_of_groups[index_of_merging_group].merge_group(group_to_check)
+
         smallest_group_i = find_smallest_group_index(list_of_groups)
-        group_to_check_index = -1
-        while list_of_groups[smallest_group_i].size() < k:
-            for i, group in enumerate(list_of_groups):
-                if group.size() < k:
-                    group_to_check_index = i
-                    break
-
-            group_to_check = list_of_groups.pop(group_to_check_index)
-
-            index_of_merging_group = find_index_of_group_to_be_merged(group_to_check, list_of_groups, min_max_diff)
-            list_of_groups[index_of_merging_group].merge_group(group_to_check)
     
     # biggest_group = find_biggest_group(list_of_groups)
 
@@ -339,36 +336,36 @@ def k_anonymity_bottom_up(table_group: Group, k: int) -> List[Group]:
         if list_of_groups[i].size() >= 2 * k:
             list_of_groups_to_be_splitted.append(list_of_groups.pop(i))
 
-    print('Groups to split: ', list_of_groups_to_be_splitted)
+    verbose('Groups to split: {}'.format(list_of_groups_to_be_splitted))
 
     while len(list_of_groups_to_be_splitted) > 0:
         group_to_be_split = list_of_groups_to_be_splitted.pop()
 
-        # group_to_be_split.size == 14, k == 6
-        print('Current splitting group: ', group_to_be_split)
+        # group_to_be_split.size == 14, k == 4
+        verbose('Current splitting group: {} {}'.format(group_to_be_split.size(), group_to_be_split))
         parts_into_split = int(group_to_be_split.size()/k)
-        # 14/6 = 2
-        # 15/6 = 2
-        # 16/6 = 2
-        print('Parts into group has to be split: ', parts_into_split)
+        verbose('Parts into group has to be split: {}'.format(parts_into_split))
         dim_of_splitted_parts = int(group_to_be_split.size()/parts_into_split)
-        # 14/2 = 7
-        # 15/2 = 7
-        # 16/2 = 8
-        print('Dimension of parts into the group has to be split: ', dim_of_splitted_parts)
+        verbose('Dimension of parts into the group has to be split: '.format(dim_of_splitted_parts))
 
         for p in range(parts_into_split):
             index = group_to_be_split.size() - 1
             splitted_group = create_empty_group()
 
             for g in range(dim_of_splitted_parts):
-                row, row_id, row_pr_val = group_to_be_split.get_all_attrs_at_index(index)
+                row, row_id, row_pr_val = group_to_be_split.pop_row(index)
                 splitted_group.add_row_to_group(row, row_id, row_pr_val)
-                group_to_be_split.pop_row(index)
-                print('Splitting group: ', splitted_group, ',at index: ', index)
+                verbose('Splitting group: {}, at index: {}'.format(splitted_group, index))
                 index -= 1
-            list_of_groups.append(splitted_group)
 
-    print('Updated list (after splitting): ', len(list_of_groups), ' ', list_of_groups)
+            if p == parts_into_split - 1:
+                while group_to_be_split.size() > 0:
+                    row, row_id, row_pr_val = group_to_be_split.pop_row(index)
+                    splitted_group.add_row_to_group(row, row_id, row_pr_val)
+                list_of_groups.append(splitted_group)
+            else:
+                list_of_groups.append(splitted_group)
+
+    verbose('Updated list (after splitting): {} {}'.format(len(list_of_groups), list_of_groups))
 
     return list_of_groups
--- src/performance_tests.py
+++ src/performance_tests.py
@@ -16,8 +16,10 @@ def instant_value_loss(groups: List[Group]):
 def run_all_tests():
 
     k_values = [3, 4, 5, 6, 7, 8, 9, 10]
+    # k_values = [5]
     # k_values = [9, 10]
-    p_values = [2, 3, 4, 5]
+    # p_values = [2, 3, 4, 5]
+    p_values = [2]
     # p_values = [2, 3, 4, 5]
     # p_values = [3, 4]
     pr_len = 4
@@ -27,6 +29,7 @@ def run_all_tests():
     df = load_data_from_file(path_to_file)
     df = remove_rows_with_nan(df)
     df = remove_outliers(df, max_stock_value=5000)
+    df = reduce_dataframe(df, companies_count=50, attributes_count=20)
     # df = reduce_dataframe(df, companies_count=30)
 
     group = create_group_from_pandas_df(df)
@@ -37,7 +40,7 @@ def run_all_tests():
 
     times = pd.DataFrame(columns=k_values, index=p_values)
     # TODO: remove verbose()
-    setverbose()
+    # setverbose()
     for k in k_values:
         for p in p_values:
             # TODO: what exactly is: 2 rows were suppressed: they could not be merged.
