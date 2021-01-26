e15cdcd5bbf4d66e11b1c5fac57058d48b76d5d0
mbrozzo <mbrozzo@gmail.com>
2021-01-26 15:56:42 +0100

    Converted most of p-anonimity and Node verbose output to debug output

--- src/kp_anonymity.py
+++ src/kp_anonymity.py
@@ -148,6 +148,7 @@ def parse_arguments():
     
     if args['debug']:
         setdebug()
+        setverbose()
     else:
         unsetdebug()
     
--- src/p_anonymity.py
+++ src/p_anonymity.py
@@ -2,7 +2,7 @@ from typing import Dict, List
 
 from group import Group
 from node import Node, create_node_from_group, merge_child_nodes, SAX
-from verbose import verbose
+from verbose import verbose, debug
 
 
 def compute_pattern_similarity(n1: Node, n2: Node) -> float:
@@ -12,7 +12,7 @@ def compute_pattern_similarity(n1: Node, n2: Node) -> float:
     between the two characters in the same position of the two PRs.
     The similarity is 1 - difference.
     """
-    verbose('Computing similarity of patterns "{}" (node {}, level {}) and "{}" (node {}, level {})'.format(n1.pr, n1.id, n1.level, n2.pr, n2.level, n2.id))
+    debug('Computing similarity of patterns "{}" (node {}, level {}) and "{}" (node {}, level {})'.format(n1.pr, n1.id, n1.level, n2.pr, n2.level, n2.id))
     if n1.pr_len() != n2.pr_len():
         raise ValueError("Pattern similarity cannot be computed on PR of different lengths")
     
@@ -32,7 +32,7 @@ def compute_pattern_similarity(n1: Node, n2: Node) -> float:
     
     diff = diff / len(n1.pr)
     sim = 1 - diff
-    verbose("Computed similarity: {}".format(sim))
+    debug("Computed similarity: {}".format(sim))
     return sim
 
 
@@ -59,52 +59,52 @@ def create_p_anonymity_tree(group: Group, p: int, max_level: int, pr_len: int) -
 
     # Node splitting
     while nodes_to_process:
-        verbose("New nodes to process found")
+        debug("New nodes to process found")
         new_nodes_to_process = []
         for n in nodes_to_process:
             n_id = n.id
             n_size = n.size()
             if n_size < p:
-                verbose("Node {} labeled bad leaf (size: {})".format(n_id, n_size))
+                debug("Node {} labeled bad leaf (size: {})".format(n_id, n_size))
                 bad_leaves.append(n)
             elif n.level == max_level:
-                verbose("Node {} labeled good leaf for reaching maximum PR level (size: {})".format(n_id, n_size))
+                debug("Node {} labeled good leaf for reaching maximum PR level (size: {})".format(n_id, n_size))
                 good_leaves.append(n)
             elif n_size < 2 * p:
-                verbose("Node {} labeled good leaf for size (size: {})".format(n_id, n_size))
+                debug("Node {} labeled good leaf for size (size: {})".format(n_id, n_size))
                 good_leaves.append(n)
                 n.maximize_level(max_level)
             else:
-                verbose("Node {} big enough to be split (size: {})".format(n_id, n_size))
+                debug("Node {} big enough to be split (size: {})".format(n_id, n_size))
                 child_nodes = n.split()
                 # Split not possible
                 if len(child_nodes) < 2 or max(child.size() for child in child_nodes) < p:
-                    verbose("Node {} labeled good leaf: split produced only one node or no child node had size >= p".format(n_id))
+                    debug("Node {} labeled good leaf: split produced only one node or no child node had size >= p".format(n_id))
                     good_leaves.append(n)
                 # Split possible
                 else:
-                    verbose("Split was successful")
+                    debug("Split was successful")
                     TG_nodes: List[Node] = []
                     TB_nodes: List[Node] = []
                     total_TB_size = 0
-                    verbose("Checking tentative node sizes:")
+                    debug("Checking tentative node sizes:")
                     for child in child_nodes:
                         if child.size() < p:
                             TB_nodes.append(child)
                             total_TB_size += child.size()
-                            verbose("Node {} is a tentative bad node (size: {}), total TB nodes size: {}".format(child.id, child.size(), total_TB_size))
+                            debug("Node {} is a tentative bad node (size: {}), total TB nodes size: {}".format(child.id, child.size(), total_TB_size))
                         else:
                             TG_nodes.append(child)
-                            verbose("Node {} is a tentative good node (size: {})".format(child.id, child.size()))
+                            debug("Node {} is a tentative good node (size: {})".format(child.id, child.size()))
 
                     new_nodes_to_process.extend(TG_nodes)
 
                     if total_TB_size >= p:
-                        verbose("Tentative bad nodes are big enough to be merged")
+                        debug("Tentative bad nodes are big enough to be merged")
                         child_merge = merge_child_nodes(TB_nodes)
                         new_nodes_to_process.append(child_merge)
                     else:
-                        verbose("Tentative bad nodes are not big enough to be merged")
+                        debug("Tentative bad nodes are not big enough to be merged")
                         new_nodes_to_process.extend(TB_nodes)
         nodes_to_process = new_nodes_to_process
 
@@ -126,14 +126,14 @@ def postprocess(good_leaves: List[Node], bad_leaves: List[Node]) -> List[Node]:
         good = good_leaves[0]
         verbose("Only 1 good leaf (node {}): adding all bad leaf rows to the only good leaf".format(good.id))
         for bad in bad_leaves:
-            verbose("Processing bad leaf {}".format(bad.id))
-            verbose("Similarity to good node: {})".format(compute_pattern_similarity(bad, good)))
+            debug("Processing bad leaf {}".format(bad.id))
+            debug("Similarity to good node: {})".format(compute_pattern_similarity(bad, good)))
             good.extend_table_with_node(bad)
     else:
         verbose("Starting postprocessing phase")
         bad_leaves.sort(key=lambda node: node.size())
         for bad in bad_leaves:
-            verbose("Processing bad leaf {}".format(bad.id))
+            debug("Processing bad leaf {}".format(bad.id))
             max_similarity = None
             most_similar_good = None
             for good in good_leaves:
@@ -141,10 +141,10 @@ def postprocess(good_leaves: List[Node], bad_leaves: List[Node]) -> List[Node]:
                 if max_similarity is None or similarity > max_similarity:
                     max_similarity = similarity
                     most_similar_good = good
-                    verbose("Updating most similar node to node {} (better similarity: {})".format(most_similar_good.id, max_similarity))
+                    debug("Updating most similar node to node {} (better similarity: {})".format(most_similar_good.id, max_similarity))
                 elif similarity == max_similarity and good.size() < most_similar_good.size():
                     most_similar_good = good
-                    verbose("Updating most similar node to node {} (smaller size: {})".format(most_similar_good.id, most_similar_good.size()))
+                    debug("Updating most similar node to node {} (smaller size: {})".format(most_similar_good.id, most_similar_good.size()))
             most_similar_good.extend_table_with_node(bad)
 
     verbose("The postprocessing phase produced the following good leaves:")
@@ -158,6 +158,7 @@ def recycle_bad_leaves(good_leaves: List[Node], bad_leaves: List[Node], p: int) 
     "Recycle bad leaves" step of the KAPRA algorithm, which merges bad leaves creating good ones.
     This function returns a list of all good leaf nodes.
     """
+    verbose('Starting "recycle bad leaves" phase')
     # If there are no bad leaves, then this step is not needed
     if not bad_leaves:
         verbose('No bad leaves found: no "recycle bad leaves" step needed')
@@ -165,14 +166,14 @@ def recycle_bad_leaves(good_leaves: List[Node], bad_leaves: List[Node], p: int) 
 
     # Preparation: bad leaves are sorted in different lists depending on their level
     bad_leaves_by_level: Dict[int, List[Node]] = {}
-    verbose("Sorting bad leaves by level:")
+    debug("Sorting bad leaves by level:")
     for bad_leaf in bad_leaves:
         if bad_leaf.level in bad_leaves_by_level:
             bad_leaves_by_level[bad_leaf.level].append(bad_leaf)
         else:
             bad_leaves_by_level[bad_leaf.level] = [bad_leaf]
     for level in bad_leaves_by_level:
-        verbose("Level {} bad leaves: {}".format(level, [n.id for n in bad_leaves_by_level[level]]))
+        debug("Level {} bad leaves: {}".format(level, [n.id for n in bad_leaves_by_level[level]]))
     
     current_level = max(bad_leaves_by_level)
 
@@ -181,17 +182,17 @@ def recycle_bad_leaves(good_leaves: List[Node], bad_leaves: List[Node], p: int) 
         if level not in bad_leaves_by_level:
             bad_leaves_by_level[level] = []
 
-    verbose("Maximum bad leaf level: {}".format(current_level))
+    debug("Maximum bad leaf level: {}".format(current_level))
     bad_rows = sum(bad_leaf.size() for bad_leaf in bad_leaves)
     while bad_rows >= p:
-        verbose("{} bad rows to process".format(bad_rows))
-        verbose("Processing bad leaves of level {}:".format(current_level))
+        debug("{} bad rows to process".format(bad_rows))
+        debug("Processing bad leaves of level {}:".format(current_level))
         for n in bad_leaves_by_level[current_level]:
-            verbose('Leaf {}: size {}, pr "{}"'.format(n.id, n.size(), n.pr))
+            debug('Leaf {}: size {}, pr "{}"'.format(n.id, n.size(), n.pr))
         leaves_by_pr: Dict[str, Node] = {}
         for bad_leaf in bad_leaves_by_level[current_level]:
             if bad_leaf.pr not in leaves_by_pr:
-                verbose('New PR "{}" found in bad leaf {}'.format(bad_leaf.pr, bad_leaf.id))
+                debug('New PR "{}" found in bad leaf {}'.format(bad_leaf.pr, bad_leaf.id))
                 leaves_by_pr[bad_leaf.pr] = bad_leaf
             # If there are other bad leaves with the same PR, merge them
             else:
@@ -199,26 +200,33 @@ def recycle_bad_leaves(good_leaves: List[Node], bad_leaves: List[Node], p: int) 
                 verbose('Merging leaf {} to leaf {} (PR: "{}")'.format(bad_leaf.id, merging_leaf.id, merging_leaf.pr))
                 merging_leaf.extend_table_with_node(bad_leaf)
         
-        verbose("The following leaves were produced:")
+        debug("The following leaves were produced:")
         for leaf in leaves_by_pr.values():
-            verbose("Leaf {}: size {}, ids {}".format(leaf.id, leaf.size(), leaf.row_ids))
+            debug("Leaf {}: size {}, ids {}".format(leaf.id, leaf.size(), leaf.row_ids))
         
         for leaf in leaves_by_pr.values():
             # If the leaf is not smaller than p, then it is a good leaf
             if leaf.size() >= p:
                 good_leaves.append(leaf)
                 bad_rows -= leaf.size()
-                verbose("Leaf {} is a good leaf, {} bad rows remaining".format(leaf.id, bad_rows))
+                debug("Leaf {} is a good leaf, {} bad rows remaining".format(leaf.id, bad_rows))
             # Otherwise its level is decreased and it will be checked in the next step for a possible merge
             else:
-                verbose("Leaf {} is a bad leaf: its SAX level is decreased".format(leaf.id, bad_rows))
+                debug("Leaf {} is a bad leaf: its SAX level is decreased".format(leaf.id, bad_rows))
                 leaf.level -= 1
                 row = leaf.table[0]
                 leaf.pr = SAX(row, leaf.level, leaf.pr_len())
                 bad_leaves_by_level[leaf.level].append(leaf)
         current_level -= 1
 
-    print(str(bad_rows) + " rows were suppressed: they could not be merged")
+    if bad_rows == 0:
+        print("No rows were suppressed")
+    else:
+        print(str(bad_rows) + " rows were suppressed: they could not be merged")
+    
+    verbose('The "recycle bad leaves" phase produced the following good leaves:')
+    for n in good_leaves:
+        verbose('Node {}: size {}, level {}, PR "{}", ids {}'.format(n.id, n.size(), n.level, n.pr, n.row_ids))
     return good_leaves
 
 
